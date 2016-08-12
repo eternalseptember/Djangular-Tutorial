248bbae8006fd54d39bca0e4f49c53094579e09e
KQ <eternal.september@gmail.com>
2016-08-11 23:06:56 -0400

    turns out logging in was working... serializing the Post model

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from authentication.models import Account
 
+
 class Post(models.Model):
 	author = models.ForeignKey(Account)
 	content = models.TextField()
